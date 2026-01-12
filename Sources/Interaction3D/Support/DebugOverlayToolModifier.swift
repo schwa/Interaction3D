afa32b174b35b43b62de8ebd8adb9058a633be92
Jonathan Wight <jwight@mac.com>
2026-01-12 14:49:21 -0800

    Replace `environment` modifier with `border` in DebugOverlayToolModifier

--- Sources/Interaction3D/Support/DebugOverlayToolModifier.swift
+++ Sources/Interaction3D/Support/DebugOverlayToolModifier.swift
@@ -1,22 +1,10 @@
-import Foundation
 import SwiftUI
-
-private struct DebugOverlayEnabledEnvironmentKey: EnvironmentKey {
-    static var defaultValue: Bool { false }
-}
-
-public extension EnvironmentValues {
-    var debugOverlayEnabled: Bool {
-        get { self[DebugOverlayEnabledEnvironmentKey.self] }
-        set { self[DebugOverlayEnabledEnvironmentKey.self] = newValue }
-    }
-}
 
 public struct DebugOverlayToolModifier: ViewModifier {
     public init() {}
 
     public func body(content: Content) -> some View {
         content
-            .environment(\.debugOverlayEnabled, true)
+            .border(.red, width: 4)
     }
 }
